b59ab31414aa0dfcbd7a2aa006e1b28dd71ca563
thong nguyen <nguyendinhthongit@gmail.com>
2021-05-26 14:47:52 +0700

    Fix error "you are attempting to open a file of type system file" when create Installation using Setup Project - Checkout to old-simple commit -> Setup Project -> Build -> Install -> FAIL - Create a clean C# winform project -> Add simple button and message box -> Build -> Install -> Still FAIL - Clone my old project "Read_shop_product\product_text_filter" that can Run after Installation: Merge the code from 'newReadExcel' to this project 'ipp_cmd_tool'

---
+++ ipp_cmd_tool/product_filter1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace product_filter1
+{
+    partial class ipp_cmd_tool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_filepath = new System.Windows.Forms.TextBox();
+            this.btn_select_file = new System.Windows.Forms.Button();
+            this.txt_input_serial = new System.Windows.Forms.TextBox();
+            this.txt_show = new System.Windows.Forms.TextBox();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.btn_copy = new System.Windows.Forms.Button();
+            this.txt_status = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_filepath
+            // 
+            this.txt_filepath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_filepath.Location = new System.Drawing.Point(12, 40);
+            this.txt_filepath.Name = "txt_filepath";
+            this.txt_filepath.Size = new System.Drawing.Size(551, 26);
+            this.txt_filepath.TabIndex = 6;
+            // 
+            // btn_select_file
+            // 
+            this.btn_select_file.Location = new System.Drawing.Point(589, 43);
+            this.btn_select_file.Name = "btn_select_file";
+            this.btn_select_file.Size = new System.Drawing.Size(75, 23);
+            this.btn_select_file.TabIndex = 7;
+            this.btn_select_file.Text = "button1";
+            this.btn_select_file.UseVisualStyleBackColor = true;
+            this.btn_select_file.Click += new System.EventHandler(this.btn_select_file_Click);
+            // 
+            // txt_input_serial
+            // 
+            this.txt_input_serial.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_input_serial.Location = new System.Drawing.Point(44, 93);
+            this.txt_input_serial.MaxLength = 10;
+            this.txt_input_serial.Name = "txt_input_serial";
+            this.txt_input_serial.Size = new System.Drawing.Size(407, 35);
+            this.txt_input_serial.TabIndex = 8;
+            this.txt_input_serial.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_input_serial_KeyDown);
+            // 
+            // txt_show
+            // 
+            this.txt_show.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_show.Location = new System.Drawing.Point(23, 154);
+            this.txt_show.Name = "txt_show";
+            this.txt_show.Size = new System.Drawing.Size(540, 31);
+            this.txt_show.TabIndex = 9;
+            // 
+            // btn_read
+            // 
+            this.btn_read.Enabled = false;
+            this.btn_read.Location = new System.Drawing.Point(579, 103);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(75, 23);
+            this.btn_read.TabIndex = 10;
+            this.btn_read.Text = "Read MAC";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            this.btn_read.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btn_read_KeyDown);
+            // 
+            // btn_copy
+            // 
+            this.btn_copy.Location = new System.Drawing.Point(589, 162);
+            this.btn_copy.Name = "btn_copy";
+            this.btn_copy.Size = new System.Drawing.Size(75, 23);
+            this.btn_copy.TabIndex = 11;
+            this.btn_copy.Text = "Copy CMD";
+            this.btn_copy.UseVisualStyleBackColor = true;
+            this.btn_copy.Click += new System.EventHandler(this.btn_copy_Click);
+            // 
+            // txt_status
+            // 
+            this.txt_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_status.Location = new System.Drawing.Point(105, 216);
+            this.txt_status.Name = "txt_status";
+            this.txt_status.ReadOnly = true;
+            this.txt_status.Size = new System.Drawing.Size(422, 29);
+            this.txt_status.TabIndex = 13;
+            this.txt_status.Text = "STATUS";
+            // 
+            // ipp_cmd_tool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 266);
+            this.Controls.Add(this.txt_status);
+            this.Controls.Add(this.btn_copy);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.txt_show);
+            this.Controls.Add(this.txt_input_serial);
+            this.Controls.Add(this.btn_select_file);
+            this.Controls.Add(this.txt_filepath);
+            this.Name = "ipp_cmd_tool";
+            this.Text = "ipphone command tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.ipp_cmd_tool_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_filepath;
+        private System.Windows.Forms.Button btn_select_file;
+        private System.Windows.Forms.TextBox txt_input_serial;
+        private System.Windows.Forms.TextBox txt_show;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_copy;
+        private System.Windows.Forms.TextBox txt_status;
+    }
+}
+
---
+++ ipp_cmd_tool/product_filter1/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Linq;
+namespace product_filter1
+{
+    public partial class ipp_cmd_tool : Form
+    {
+        //private List<Product> productList = new List<Product>();
+        //string strStart;
+        //string strEnd;
+        //string product_temp;
+        //int index;
+        //bool stop = false;
+
+        public static int rowCount, colCount;
+        public static int startCol, endCol;
+        public static string[,] excel_values;
+        Excel.Application xlApp;
+        Excel.Workbook xlWorkbook;
+        Excel._Worksheet xlWorksheet;
+        Excel.Range xlRange;
+        string file_path;
+        string io_dir_text;
+        /*
+         
+         https://stackoverflow.com/questions/7462748/how-to-run-code-when-form-is-shown
+        */
+        string temp_txt_path;
+        public ipp_cmd_tool()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+        //private void bt_do_Click(object sender, EventArgs e)
+        //{
+        //    {
+                
+        //       // string text_read = txt_1.Text;
+        //        strStart = "<a href=\"/products/";
+        //        strEnd = "</em>";
+        //        while (stop == false)
+        //        {
+        //            product_temp = getBetween(text_read, strStart, strEnd);
+        //            if (product_temp == "")
+        //            {
+        //                stop = true;
+        //                break;
+        //            }
+        //            //code pro add to product list
+        //            newproduct(product_temp);
+        //            txt_2.Text += product_temp;
+        //            txt_2.Text += Environment.NewLine;
+        //            index = text_read.IndexOf(strEnd) + strEnd.Length;
+        //            text_read = text_read.Substring(index);
+        //        }
+        //    }
+        //}
+        //private void newproduct(string product_temp)
+        //{
+        //    string price, name;
+        //    Product newProduct = new Product();
+        //    name = getBetween(product_temp, "title=\"", "\"");
+        //    newProduct.Name = name;
+        //    newProduct.Image_url = getBetween(product_temp, "src=\"//", "\"");
+        //    newProduct.Product_url = getBetween(product_temp, "<strong><a href='", "'>");
+        //    price = getBetween(product_temp, "\"price\"><em>", "₫");
+        //    price = price.Replace(" ", "");
+        //    newProduct.Price = price;
+        //    productList.Add(newProduct);
+        //}
+
+        //private void bt_excel_Click(object sender, EventArgs e)
+        //{
+        // //   toExcelFile();
+        //}
+       // public static int rowCount, colCount;
+       // public static string[,] decode;
+
+        private void txt_2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void txt_input_serial_serial_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //}
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            txt_show.Text = "";
+            string input_txt_read = txt_input_serial.Text;
+            string[] decode_first_col = new string[rowCount];
+            /*
+             * https://stackoverflow.com/questions/27427527/how-to-get-a-complete-row-or-column-from-2d-array-in-c-sharp
+             * To get a complete row out of an multi-dimensional array, you have to loop
+             */
+            for (int k = 0; k < rowCount; k++)
+            {
+                decode_first_col[k] = excel_values[k, startCol];
+                // Console.WriteLine(decode_first_col[k].ToString());
+            }
+            int index = Array.IndexOf(decode_first_col, input_txt_read);
+            if (decode_first_col.Contains(input_txt_read))
+            {
+               // lb_out_stt.Text = "The output value for input " + input_txt_read + " is:";
+                txt_show.Text = excel_values[index, startCol + 1];
+                btn_copy.Enabled = true;
+                /*
+                 Position the Cursor at the Beginning or End of Text in a TextBox Control
+                 */
+                Clipboard.SetText(txt_show.Text);
+                txt_status.Text = "Read thanh cong";
+            }
+            else
+            {
+            //    lb_out_stt.Text = "Can't find a match";
+                txt_show.Text = "NaN";
+                txt_status.Text = "Nan";
+            }
+            txt_input_serial.Text = "2019-0";
+            txt_input_serial.Select(txt_input_serial.Text.Length, 0);
+        }
+
+        private void btn_copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txt_show.Text);
+            txt_show.Text = "";
+            btn_copy.Enabled = false;
+            txt_status.Text = "Copy thanh cong";
+        }
+
+        private void ipp_cmd_tool_Shown(object sender, EventArgs e)
+        {
+            temp_txt_path = Application.StartupPath + @"..\..\..\..\" + @"working_dir_path.txt";
+            io_dir_text = System.IO.File.ReadAllText(temp_txt_path);
+            txt_filepath.Text = io_dir_text;
+            file_path = io_dir_text;
+            txt_input_serial.Text = "2019-0";
+            readExcelFile();
+            /*
+             move this code block to here to be effective
+             */
+            startCol = 1;
+            endCol = 3;
+        }
+
+        private void btn_read_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_read_Click(this, new EventArgs());
+            }
+        }
+
+        private void txt_input_serial_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_read_Click(this, new EventArgs());
+            }
+        }
+
+        private void btn_select_file_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\Users\nguye\Downloads\Excel_file_ws\fileio_excel_ws\newReadExcel";
+            /*
+             To be opened Supported files
+             */
+            openFileDialog1.Filter = "Excel (*.xlsx)|*.xlsx";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog1.FileName != file_path)
+                {
+                    btn_read.Enabled = false;
+                    file_path = openFileDialog1.FileName;
+                    txt_filepath.Text = openFileDialog1.FileName;
+                    /*
+                        if file dir is changed, perform readExcel
+                     */
+                    readExcelFile();
+                    /*
+                        https://www.c-sharpcorner.com/article/c-sharp-write-to-file/
+                        Write string to the target text file
+                     */
+                    File.WriteAllText(temp_txt_path, file_path);
+
+                }
+            }
+        }
+
+
+        public void readExcelFile()
+        {
+            //Create COM Objects. Create a COM object for everything that is referenced
+            xlApp = new Excel.Application();
+            if (System.IO.File.Exists(file_path))
+            {
+                xlWorkbook = xlApp.Workbooks.Open(file_path, ReadOnly: true, Password: "m3e");
+                xlWorksheet = xlWorkbook.Sheets[1];
+                xlRange = xlWorksheet.UsedRange;
+                rowCount = xlRange.Rows.Count;
+                colCount = xlRange.Columns.Count;
+                // Console.WriteLine(rowCount.ToString());
+                //  Console.WriteLine(colCount.ToString());
+                excel_values = new string[rowCount, colCount];
+                //iterate over the rows and columns and print to the console as it appears in the file
+                //excel is not zero based!!
+                txt_status.Text = "Start reading data from a new excel file";
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    for (int j = 1; j <= colCount; j++)
+                    {
+                        if (xlRange.Cells[i, j] != null && ((Excel.Range)xlRange.Cells[i, j]).Value2 != null)
+                        {
+                            excel_values[i - 1, j - 1] = ((Excel.Range)xlRange.Cells[i, j]).Value2.ToString();
+                            // Console.WriteLine(excel_values[i - 1, j - 1].ToString());
+                        }
+                    }
+                }
+                txt_status.Text = "Reading new file done";
+                //  Console.WriteLine("Reading new file done");
+                //cleanup
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                /*
+                 * rule of thumb for releasing com objects:
+                 * never use two dots, all COM objects must be referenced and released individually
+                  * ex: [somthing].[something].[something] is bad
+                */
+                //Console.WriteLine(excel_values[0, 0] + excel_values[1, 1]);
+                //close and release
+                xlWorkbook.Close();
+                //quit and release
+                xlApp.Quit();
+            }
+            btn_read.Enabled = true;
+        }
+
+        //public void toExcelFile()
+        //{
+        //    //Create COM Objects. Create a COM object for everything that is referenced
+        //    Excel.Application xlApp = new Excel.Application();
+        //    Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(Application.StartupPath + @"\Database.xlsx");
+        //    //Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"E:\Cuahang_ap\Database.xlsx");
+        //    Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+        //    Excel.Range xlRange = xlWorksheet.UsedRange;
+        //    decode = new string[rowCount, colCount];
+        //    rowCount = xlRange.Rows.Count;
+        //    colCount = xlRange.Columns.Count;
+        //    //iterate over the rows and columns and print to the console as it appears in the file
+        //    //excel is not zero based!!
+        //    for (int i = rowCount + 1; i <= rowCount + productList.Count; i++)
+        //    {
+        //        xlRange.Cells[i, 1].Value2 = productList[i-rowCount-1].Name;
+        //        xlRange.Cells[i, 2].Value2 = productList[i-rowCount-1].Image_url;
+        //        xlRange.Cells[i, 3].Value2 = productList[i-rowCount-1].Product_url;
+        //        xlRange.Cells[i, 4].Value2 = productList[i-rowCount-1].Price;
+        //    }
+
+        //    //cleanup
+        //    GC.Collect();
+        //    GC.WaitForPendingFinalizers();
+        //    //rule of thumb for releasing com objects:
+        //    //  never use two dots, all COM objects must be referenced and released individually
+        //    //  ex: [somthing].[something].[something] is bad
+        //  //  Console.WriteLine(decode[0, 0] + decode[1, 1]);
+        //    //close and release
+        //    xlWorkbook.Close();
+        //    //quit and release
+        //    xlApp.Quit();
+        //}
+    }
+}
